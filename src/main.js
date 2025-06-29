73fcca4197e579451182fc6fa86c796ab840e036
vitor <vitor.grosskopf@gmail.com>
2025-06-29 15:38:05 -0300

    Updating player movement and sprites display

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 import { scaleFactor } from "./constant.js";
 import { k } from "./kaboomCtx.js"
+import { displayDialogue } from "./utils.js";
 
-k.loadSprite("spritesheet", "./spritesheet.png", {
-  sliceX: 67,
-  sliceY: 21,
+k.loadSprite("spritesheet", "./TESTE.png", {
+  sliceX: 39,
+  sliceY: 31,
   anims: {
-    "idle-down": 3348,
-    "walk-down": { from: 3348, to: 3350, loop: true, speed: 8 },
-    "idle-side": 3356,
-    "side-walk": { from: 3356, to: 3357, loop: true, speed: 8 },
-    "idle-up": 3351,
-    "up-walk": { from: 3351, to: 3353, loop: true, speed: 8 }
+    "idle-down": 936,
+    // "idle-down": 936,
+    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+    "idle-side": 975,
+    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+    "idle-up": 1014,
+    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
   },
 });
+// anims: {
+  // "walk-down": { from: 3348, to: 3350, loop: true, speed: 8 },
+  // "idle-side": 3356,
+  // "side-walk": { from: 3356, to: 3357, loop: true, speed: 8 },
+  // "idle-up": 3351,
+  // "up-walk": { from: 3351, to: 3353, loop: true, speed: 8 }
+  // },
+// });
 
 k.loadSprite("map", "./map.png");
 
@@ -21,7 +31,8 @@ k.setBackground(k.Color.fromHex("#311047"));
 k.scene("main", async () => {
   const mapData = await (await fetch("./map.json")).json();
   const layers = mapData.layers;
-  const map = k.make([
+
+  const map = k.add([
     k.sprite("map"),
     k.pos(0),
     k.scale(scaleFactor)
@@ -45,7 +56,7 @@ k.scene("main", async () => {
   ]);
 
   for (const layer of layers) {
-    if (layer.name === "border") {
+    if (layer.name === "boundaries") {
       for (const boundary of layer.objects) {
         map.add([
           k.area({
@@ -58,12 +69,37 @@ k.scene("main", async () => {
         if (boundary.name) {
           player.onCollide(boundary.name, () => {
             player.isInDialogue = true;
-            // to do
+            displayDialogue("TODO", () => (player.isInDialogue = false))
           });
+        }
+      }
+      continue;
+    }
+
+    if (layer.name === "spawnpoints") {
+      for (const entity of layer.objects) {
+        if (entity.name === "player") {
+          player.pos = k.vec2(
+            (map.pos.x + entity.x) * scaleFactor,
+            (map.pos.y + entity.y) * scaleFactor
+          );
+          k.add(player);
+          continue;
         }
       }
     }
   }
+
+  k.onUpdate(() => {
+    k.camPos(player.pos.x, player.pos.y + 100);
+  })
+
+  k.onMouseDown((mouseBtn) => {
+    if (mouseBtn !== "left" || player.isInDialogue) return;
+
+    const worldMousePos = k.toWorld(k.mousePos());
+    player.moveTo(worldMousePos, player.speed);
+  });
 });
 
  k.go("main")
