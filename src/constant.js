e7c9e9e68205874f13cf59316b8d338d8a1f9c9b
vitor <vitor.grosskopf@gmail.com>
2025-06-30 12:41:36 -0300

    Adds dialogue for bookshelfs

--- src/constant.js
+++ src/constant.js
@@ -8,12 +8,12 @@ export const dialogueData = {
 
       "We are, we aree.. on a cruize we are!"
       - I'll be the pirate king!"`,
-  tv: `... `,
+  tv: `... Get to the ChoOOPPAA... ... I'll be back...`,
   pc: `Log in... Password: 123456 \n A place to rest, and play my games.\n
     I love action RPG.
     DARK SOULS 🛡️🗡🔥💀𒉭: "PAISE THE SUN!🌞 " `,
   exit: `To Exit just close this tab`,
-  shelf: `Half utility, half personality. Tech gadgets I don't use enough, collectibles I refuse to throw away, and maybe a plant I'm trying not to kill. It’s a snapshot of the stuff that matters (or once did)`,
+  shelf: `Half utility, half personality. Tech gadgets I don't use enough, collectibles I refuse to throw away, and maybe a plant I'm trying not to kill. It's a snapshot of the stuff that matters (or once did)`,
   alexa: `Alexa! play my favorite <a href="https://open.spotify.com/track/2BHj31ufdEqVK5CkYDp9mA?si=315d48c2d69d4023" target="_blank">song</a>`,
   stove: `Here is where I cook my favorite meals, mostly pasta! <3`,
   sink: `Obviously, keep it clean`,
@@ -23,9 +23,11 @@ export const dialogueData = {
   fridge: `My fuel chamber. It's mostly leftovers, eggs, and Monster Energy drinks. I check it way too often`,
   "office-book": `Here are some special ones, the ones I've cried reading... Calculus, Linear Algebra, Inorganic Chemistry...`,
   win2: "OHH look a bird! 🐦",
+  win: `Looks like it's gonna rain`,
   "room-book": `This one is a bit messy... sorry... I can recommend you another: The hitchhiker Guide of the galaxy. By the I was born in the 42° day of the year!` ,
   cv: `Here is my <a href="https://docs.google.com/document/d/18khH88tso9JKmXe3BGYL-W-bFDeQBFqe/edit?usp=sharing&ouid=104802605946471093359&rtpof=true&sd=true" target="_blank">resume</a> You can also see it in <a href="https://docs.google.com/document/d/1SGMxWwQu6Xkw9DxLKbaSakJVrykty2OC/edit?usp=sharing&ouid=104802605946471093359&rtpof=true&sd=true" target="_blank">portuguese</a>`,
   diploma: `My Chemistry Bachelors degree <a href="https://drive.google.com/file/d/1UE-dwzN_oDJKCbHv9tP6uHFmhZ1CUzS8/view?usp=sharing" target="_blank">Diploma</a>`,
   diploma2: `My Chemistry Masters degree <a href="https://drive.google.com/file/d/1UaODHQXx9fG3f8mksqogd47EK90lFVRd/view?usp=sharing" target="_blank">Diploma</a>`,
-  pic: `A picture of my art <a href="https://www.instagram.com/p/B87FQnmnQU3/?igsh=Ym5qcDFrNGU4OHkx" target="_blank">@vitaoTM</a> `
+  pic: `A picture of my art <a href="https://www.instagram.com/p/B87FQnmnQU3/?igsh=Ym5qcDFrNGU4OHkx" target="_blank">@vitaoTM</a> `,
+  "lr-book": "A technical bookshelf, a recommendation: The pragmatic programmer",
 }
