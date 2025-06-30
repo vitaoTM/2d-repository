1d20046e0806ffa4a6b37bc144486357b1ec7b5c
vitor <vitor.grosskopf@gmail.com>
2025-06-29 22:44:51 -0300

    Fixing no movement bugs
    - Update map.json file
    - Update map.png file

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import { dialogueData, scaleFactor } from "./constant.js";
 import { k } from "./kaboomCtx.js"
 import { displayDialogue, setCamScale } from "./utils.js";
 
-k.loadSprite("spritesheet", "./TESTE.png", {
+k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
   sliceY: 31,
   anims: {
     "idle-down": 936,
-    // "idle-down": 936,
     "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
     "idle-side": 975,
     "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
@@ -25,7 +24,8 @@ k.scene("main", async () => {
   const layers = mapData.layers;
 
   const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
-  const player = k.add([
+
+  const player = k.make([
     k.sprite("spritesheet",
     { anim: "idle-down" }),
     k.area({
