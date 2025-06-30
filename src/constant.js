8f7204e82c5c7636050040545e9b4162afefd02f
vitor <vitor.grosskopf@gmail.com>
2025-06-30 00:35:51 -0300

    Adding dialogue data

--- src/constant.js
+++ src/constant.js
@@ -1,9 +1,31 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  tv: `AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH `,
-  exit: `Exit`
+  "pc-master": `This is my PC. I work mostly in Ruby on Rails these days.
+    I've made a few projects with it. I also like Python and data science. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/vitaoTM target="_blank">Github</a>!`,
+  tv2: `I've been watching One Piece...
+
+      "We are, we aree.. on a cruize we are!"
+      - I'll be the pirate king!"`,
+  tv: `... `,
+  pc: `Log in... Password: 123456 \n A place to rest, and play my games.\n
+    I love action RPG.
+    DARK SOULS 🛡️🗡🔥💀𒉭: "PAISE THE SUN!🌞 " `,
+  exit: `To Exit just close this tab`,
+  shelf: `Half utility, half personality. Tech gadgets I don't use enough, collectibles I refuse to throw away, and maybe a plant I'm trying not to kill. It’s a snapshot of the stuff that matters (or once did)`,
+  alexa: `Alexa! play my favorite <a href="https://open.spotify.com/track/2BHj31ufdEqVK5CkYDp9mA?si=315d48c2d69d4023" target="_blank">song</a>`,
+  stove: `Here is where I cook my favorite meals, mostly pasta! <3`,
+  sink: `Obviously, keep it clean`,
+  book: `Here are my book collectio, a mix of tech books, manga, and a few half-read self-help titles. My personal recommendation for  you: The richest man in Babylon`,
+  pic: ``,
+  bed: `This is where I sleep. `,
+  fridge: `My fuel chamber. It's mostly leftovers, eggs, and Monster Energy drinks. I check it way too often`,
+  "office-book": `Here are some special ones, the ones I've cried reading... Calculus, Linear Algebra, Inorganic Chemistry...`,
+  win2: "OHH look a bird! 🐦",
+  "room-book": `This one is a bit messy... sorry... I can recommend you another: The hitchhiker Guide of the galaxy. By the I was born in the 42° day of the year!` ,
+  cv: `Here is my <a href="https://docs.google.com/document/d/18khH88tso9JKmXe3BGYL-W-bFDeQBFqe/edit?usp=sharing&ouid=104802605946471093359&rtpof=true&sd=true" target="_blank">resume</a> You can also see it in <a href="https://docs.google.com/document/d/1SGMxWwQu6Xkw9DxLKbaSakJVrykty2OC/edit?usp=sharing&ouid=104802605946471093359&rtpof=true&sd=true" target="_blank">portuguese</a>`,
+  diploma: `My Chemistry Bachelors degree <a href="https://drive.google.com/file/d/1UE-dwzN_oDJKCbHv9tP6uHFmhZ1CUzS8/view?usp=sharing" target="_blank">Diploma</a>`,
+  diploma2: `My Chemistry Masters degree <a href="https://drive.google.com/file/d/1UaODHQXx9fG3f8mksqogd47EK90lFVRd/view?usp=sharing" target="_blank">Diploma</a>`,
+  pic: `A picture of my art <a href="https://www.instagram.com/p/B87FQnmnQU3/?igsh=Ym5qcDFrNGU4OHkx" target="_blank">@vitaoTM</a> `
 }
