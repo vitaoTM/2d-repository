41cd751ee80b60c5cedee117d5075db704f0510e
vitor <vitor.grosskopf@gmail.com>
2025-06-30 14:20:46 -0300

    Fixing font size overflow

--- src/constant.js
+++ src/constant.js
@@ -5,9 +5,7 @@ export const dialogueData = {
     I've made a few projects with it. I also like Python and data science. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/vitaoTM target="_blank">Github</a>!`,
   tv2: `I've been watching One Piece...
-
-      "We are, we aree.. on a cruize we are!"
-      - I'll be the pirate king!"`,
+      "- I'll be the pirate king!"`,
   tv: `... Get to the ChoOOPPAA... ... I'll be back...`,
   pc: `Log in... Password: 123456 \n A place to rest, and play my games.\n
     I love action RPG.
@@ -17,17 +15,17 @@ export const dialogueData = {
   alexa: `Alexa! play my favorite <a href="https://open.spotify.com/track/2BHj31ufdEqVK5CkYDp9mA?si=315d48c2d69d4023" target="_blank">song</a>`,
   stove: `Here is where I cook my favorite meals, mostly pasta! <3`,
   sink: `Obviously, keep it clean`,
-  book: `Here are my book collectio, a mix of tech books, manga, and a few half-read self-help titles. My personal recommendation for  you: The richest man in Babylon`,
+  book: `Here are my book collectio, a mix of tech books, manga, and a few half-read self-help titles. I suggest you: The richest man in Babylon`,
   pic: ``,
   bed: `This is where I sleep. `,
   fridge: `My fuel chamber. It's mostly leftovers, eggs, and Monster Energy drinks. I check it way too often`,
   "office-book": `Here are some special ones, the ones I've cried reading... Calculus, Linear Algebra, Inorganic Chemistry...`,
   win2: "OHH look a bird! 🐦",
   win: `Looks like it's gonna rain`,
-  "room-book": `This one is a bit messy... sorry... I can recommend you another: The hitchhiker Guide of the galaxy. By the I was born in the 42° day of the year!` ,
+  "room-book": `This one is a bit messy... sorry... I can suggest you another: The hitchhiker Guide of the galaxy. By the I was born in the 42° day of the year!` ,
   cv: `Here is my <a href="https://docs.google.com/document/d/18khH88tso9JKmXe3BGYL-W-bFDeQBFqe/edit?usp=sharing&ouid=104802605946471093359&rtpof=true&sd=true" target="_blank">resume</a> You can also see it in <a href="https://docs.google.com/document/d/1SGMxWwQu6Xkw9DxLKbaSakJVrykty2OC/edit?usp=sharing&ouid=104802605946471093359&rtpof=true&sd=true" target="_blank">portuguese</a>`,
   diploma: `My Chemistry Bachelors degree <a href="https://drive.google.com/file/d/1UE-dwzN_oDJKCbHv9tP6uHFmhZ1CUzS8/view?usp=sharing" target="_blank">Diploma</a>`,
   diploma2: `My Chemistry Masters degree <a href="https://drive.google.com/file/d/1UaODHQXx9fG3f8mksqogd47EK90lFVRd/view?usp=sharing" target="_blank">Diploma</a>`,
   pic: `A picture of my art <a href="https://www.instagram.com/p/B87FQnmnQU3/?igsh=Ym5qcDFrNGU4OHkx" target="_blank">@vitaoTM</a> `,
-  "lr-book": "A technical bookshelf, a recommendation: The pragmatic programmer",
+  "lr-book": "A technical bookshelf, I suggest: The pragmatic programmer",
 }
